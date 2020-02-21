81ebf34bc8ff35e32f5c8cef5465456a0c6dd53e
Patrick LaRocque <plarocque@mitre.org>
2020-02-21 15:31:37 -0500

    [496] Rearrange CRD / DTR resource files.

---
+++ setupfolders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# pass in the folder in which you'd like to create the structure for
+echo "Create Folder Structure for $1"
+
+mkdir $1
+mkdir $1/R4
+mkdir $1/R4/resources
+mkdir $1/R4/files
+mkdir $1/STU3
+mkdir $1/STU3/resources
+mkdir $1/STU3/files
+
